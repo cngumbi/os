698614ab0f8c8888388bdac45efd976851f3edc2
cngumbi35 <32173445+cngumbi@users.noreply.github.com>
2018-09-25 13:41:40 +0300

    Create simple-antivirus.cpp

---
+++ simple-antivirus.cpp
@@ -0,0 +1,11 @@
+# Cstreakstudio
+//C programming code simple anti-virus
+#include<windows.h>
+#include<iostream>
+main(){
+//write the massage to be displayed of the screeen
+std::cout<<"Scanning the system...!"<<endl;
+//write the location of tyhe file that contains virus
+remove(c:/user/desktop/index.php);
+std::cout<<"virus deleted seccessfully..!";
+}
